9c8a4410942b2fdb359f11e00d4075d97b7b1099
Dekuzzo02 <dicuzzo8@gmail.com>
2021-04-20 22:13:20 +0000

    3

--- client-socketio/src/app/app.module.ts
+++ client-socketio/src/app/app.module.ts
@@ -1,13 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
-import { SocketIoModule, SocketIoConfig } from 'ngx-socket-io';
-import { FormsModule } from '@angular/forms';
+import { SocketIoModule, SocketIoConfig } from 'ngx-socket-io';  //libreria importata
 import { SocketService } from './socket.service';
 import { CesarService } from './cesar.service';
+import { CryptoService } from './crypto.service';
+import { FormsModule } from '@angular/forms';
 
-const config: SocketIoConfig = { url: 'http://localhost:3000', options: {/*transport : ['websocket'], withCredentials:false*/} };
+const config: SocketIoConfig = { url: 'https://3000-salmon-pelican-6w7si3qh.ws-eu03.gitpod.io', options: {} };  //url del server
 
 @NgModule({
   declarations: [
@@ -15,11 +15,10 @@ const config: SocketIoConfig = { url: 'http://localhost:3000', options: {/*trans
   ],
   imports: [
     BrowserModule,
-    SocketIoModule.forRoot(config),
+    SocketIoModule.forRoot(config), //anche qui
     FormsModule
   ],
-  providers: [SocketService, CesarService],
+  providers: [SocketService, CesarService, CryptoService],  //aggiunti i service
   bootstrap: [AppComponent]
-
 })
 export class AppModule { }
